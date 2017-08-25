289783d7657f4de8c95e43bedf721d8965648107
chickinnick <chickinnick@ya.ru>
2017-08-25 18:09:02 +0300

    custom carousel

--- js/script.js
+++ js/script.js
@@ -41,104 +41,10 @@ $(function () {
 		}, 800);
 	});
 });
-/*Карусель*/
-var data = [{
-	"id": 0
-	, "name": "P"
-}, {
-	"id": 1
-	, "name": "E"
-}, {
-	"id": 2
-	, "name": "F"
-}];
-var data1 = [
-	{
-		"id": 3
-		, "name": "P"
-}, {
-		"id": 4
-		, "name": "E"
-}, {
-		"id": 5
-		, "name": "F"
-}]
-var data2 = [
-	{
-		"id": 3
-		, "name": "P"
-}, {
-		"id": 4
-		, "name": "E"
-}, {
-		"id": 5
-		, "name": "F"
-}];
-
-function initCarusel(data, data1, data2) {
-	createItems(data, true);
-	createItems(data1, false);
-	createItems(data2, false);
-}
-
-function createItems(data, isActive) {
-	var slideContainer = document.getElementById("slideContainer");
-	var slide = document.createElement('div');
-	if (isActive) {
-		slide.className = "item active";
-	}
-	else {
-		slide.className = "item";
-	}
-	for (var i = 0; i < data.length; i++) {
-		slide.appendChild(createItem(data[i]));
-	}
-	slideContainer.appendChild(slide);
-}
-
-function createItem(person) {
-	var listButton = document.createElement('div');
-	listButton.className = "category-tileWorks col-lg-4 col-md-4 col-sm-4 col-xs-4 col-xxs-4";
-	var text = "<b>ID:</b> " + person.id + "<br>";
-	text = text + "<b>Name:</b> " + person.name;
-	listButton.innerHTML = text;
-	var image = document.createElement('img');
-	image.className = "image";
-	listButton.appendChild(image);
-	var button = document.createElement('button');
-	button.className = "listButto";
-	button.style.display = "none";
-	listButton.appendChild(button);
-	button.addEventListener("click", function () {
-		window.open("https://www.w3schools.com");
-	}, false);
-	listButton.addEventListener("mouseover", function () {
-		button.style.display = "inline-block";
-	}, false);
-	listButton.addEventListener("mouseout", function () {
-		button.style.display = "none";
-	}, false);
-	return listButton;
-}
-initCarusel(data, data1, data2);
-var datawork = [{
-	"title": "Cinamaker"
-	, "texts": "Я живу в Харькове"
-}, {
-	"title": "Irny"
-	, "texts": "Я жиыу в Украине"
-}];
 //	datawork.forEach(function(person){
 //		div.appendChild(person);
 //	})
 //}
-function b(datawork) {
-	var mainContainer = document.getElementById("mainContainer");
-	for (var i = 0; i < datawork.length; i++) {
-		mainContainer.appendChild(init(datawork[i])); // ты же в функции init ничего не возвразаешь
-	}
-}
-b(datawork);
 
 function init(person) {
 	var divone = document.createElement("div");
